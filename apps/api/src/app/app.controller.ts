ff17580ddd24991b9b521ff6eaba123cb8e82d4f
Zayd Selmani <zayd448@gmail.com>
2023-08-17 14:48:28 +0200

    dsqdqs

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import * as requestIp from 'request-ip';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getData() {
-    return this.appService.getData();
+  getData(@Req() req) {
+    const yourIp = requestIp.getClientIp(req);
+    return {
+      yourIp: yourIp,
+      yourIp2: req.clientIp,
+      data: this.appService.getData(),
+    };
   }
 }
