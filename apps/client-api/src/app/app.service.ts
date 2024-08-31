4d0f22a8d9ded060a498221613203b951ee203cc
Zayd Selmani <zayd448@gmail.com>
2024-08-31 11:18:47 +0200

    two caller apps

--- apps/client-api/src/app/app.service.ts
+++ apps/client-api/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return ({ message: 'Hello Im Server api' });
+    return { message: 'Hello Im Client api' };
   }
 }
