d06f28d67c3bb626a025afe8d8207636f4b94b1d
Zayd Selmani <zayd448@gmail.com>
2024-08-31 11:33:20 +0200

    fix tests and remove nx cloud

--- apps/server-api/src/app/app.controller.spec.ts
+++ apps/server-api/src/app/app.controller.spec.ts
@@ -14,9 +14,9 @@ describe('AppController', () => {
   });
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
+    it('should return "Hello Im Server api"', () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({message: 'Hello API'});
+      expect(appController.getData()).toEqual({message: 'Hello Im Server api'});
     });
   });
 });
--- apps/server-api/src/app/app.service.spec.ts
+++ apps/server-api/src/app/app.service.spec.ts
@@ -14,8 +14,8 @@ describe('AppService', () => {
   });
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
-      expect(service.getData()).toEqual({message: 'Hello API'});
+    it('should return "Hello Im Server api"', () => {
+      expect(service.getData()).toEqual({message: 'Hello Im Server api'});
     });
   });
 });
